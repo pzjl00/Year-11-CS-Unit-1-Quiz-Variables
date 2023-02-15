89a94e0fa6b7443277b0481c9d7aeff15392ec13
phoebelam <pzjl00@student.cis.edu.hk>
2023-02-15 14:55:53 +0800

    Completed.

--- src/Variables.java
+++ src/Variables.java
@@ -3,30 +3,31 @@ public class Variables {
     public static void main(String[] args) {
 
         // 1.
-
+int age = 33;
         // 2.
-
+System.out.println(age);
         // 3.
-
+double balance = 18.89;
         // 4.
-
+int dollars = (int) balance;
         // 5.
-
+dollars = dollars + 1;
+int dollarsRounded = dollars;
         // 6.
-
+char letter = 'A';
         // 7.
-
+System.out.println(letter);
         // 8.
-
+System.out.println((int)letter);
         // 9.
-
+letter = ++letter;
         // 10.
-
+System.out.println(++letter);
         // 11.
-
+System.out.println((int)++letter);
         // 12.
         /**
-         * [type answer here]
+         * when casing the variable letter to an integer, the integer printed was the number associated with the character. For example, 65 corresponds to the letter A.
          */
 
     }
